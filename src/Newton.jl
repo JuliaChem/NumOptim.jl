864ce8054a4d5a124e4ef5faf386d33e97d81181
Kelvyn Baruc SS <kelvyn.baruc@gmail.com>
2015-12-13 16:55:42 -0600

    Newton.jl updated

--- src/Newton.jl
+++ src/Newton.jl
@@ -1,5 +1,5 @@
 using Calculus
-function NewtonOptim(f, x0; error = 0.000001, maxiter = 100, iter = true)
+function Newton(f, x0; error = 0.000001, maxiter = 100, iter = true)
 
       # Symbolic derivatives
       dfk1 = differentiate(f)
