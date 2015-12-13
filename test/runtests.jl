d42de7f3a4869c968a55034c68d1104e0e3dc72e
Kelvyn Baruc SS <kelvyn.baruc@gmail.com>
2015-12-13 17:46:50 -0600

    runtest.jl updated

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,9 @@ using OptimJulia
 using Base.Test
 
 # write your own tests here
-@test 1 == 1
+
+f = :(((((1 - 8x) + 2 * x ^ 2) - (10 / 3) * x ^ 3) + (1 / 4) * x ^ 4 + (4 / 5) * x ^ 5) - (1 / 6) * x ^ 6)
+
+for x0 = 1:10
+      Newton(f, x0)
+end
