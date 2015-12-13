a5aac52542e00aa3aeba6c50be7140df6b87ce39
Kelvyn Baruc SS <kelvyn.baruc@gmail.com>
2015-12-13 17:55:06 -0600

    fixed runtest.jl (tested only with 1 expression and single variable)

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,9 @@ using Base.Test
 # write your own tests here
 
 f = :(((((1 - 8x) + 2 * x ^ 2) - (10 / 3) * x ^ 3) + (1 / 4) * x ^ 4 + (4 / 5) * x ^ 5) - (1 / 6) * x ^ 6)
-
-for x0 = 1:10
-      Newton(f, x0)
+alg = "Newton"
+for i = 1:10
+      Newton(f, x0 = i)
+      println("=========================================")
+      println("Test $i with $alg algorithm passed")
 end
