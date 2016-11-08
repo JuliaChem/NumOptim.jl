6cfd986f9050a1523d84e22263a3bd495b1d6844
Kelvyn Baruc SS <kelvyn.baruc@gmail.com>
2016-11-07 18:26:42 -0600

    Try & catch to avoid travis error

--- src/Newton.jl
+++ src/Newton.jl
@@ -7,48 +7,56 @@
  """ ->
 
 function Newton(f, var; x0 = 1, error = 0.0001, maxiter = 100, iter = true)
-      # Symbolic derivatives using Module SymPy
-      dfk1 = diff(f, var)
-      dfk2 = diff(dfk1, var)
+      try
+            # Symbolic derivatives using Module SymPy
+            dfk1 = diff(f, var)
+            dfk2 = diff(dfk1, var)
 
-      err = Inf
-      i = 1
-      x_num = 0
+            err = Inf
+            i = 1
+            x_num = 0
 
-      # Numeric iterations
-      iter == true ? ti = time_ns() : nothing # To measure algorothm performance
-      while err > error
-            x_num = x0
+            # Numeric iterations
+            iter == true ? ti = time_ns() : nothing # To measure algorothm performance
 
-            xk = N(var |> subs(var,x_num)) - N(dfk1 |> subs(var, x_num)) / N(dfk2 |> subs(var, x_num))
-            err = abs((xk -  N(var |> subs(var,x_num))) / xk)
+            while err > error
+                  x_num = x0
 
-                  if iter == true
-                        if i == 1
-                              println("======================================================")
-                              println(" Newton Solver for Unconstrained Optimization (0.0.2)")
+                  xk = N(var |> subs(var,x_num)) - N(dfk1 |> subs(var, x_num)) / N(dfk2 |> subs(var, x_num))
+                  err = abs((xk -  N(var |> subs(var,x_num))) / xk)
+
+                        if iter == true
+                              if i == 1
+                                    println("======================================================")
+                                    println(" Newton Solver for Unconstrained Optimization (0.0.2)")
+                                    println(" ")
+                              end
+
+                              if i == 1 || ceil((i/1)/10) == (i/1)/10
+                                    println("======================================================")
+                                    @printf("Iter\t X\t\t f(x)\t\t |err|\n")
+                                    println("======================================================")
+                              end
+
+                              @printf("%i\t %4.6e\t %4.6e\t %4.6e\n", i, xk, N(f |> subs(var,x_num)), err)
                         end
 
-                        if i == 1 || ceil((i/1)/10) == (i/1)/10
-                              println("======================================================")
-                              @printf("Iter\t X\t\t f(x)\t\t |err|\n")
-                              println("======================================================")
-                        end
+                  x0 = xk
+                  x_num = xk
+                  i += 1
 
-                        @printf("%i\t %4.6e\t %4.6e\t %4.6e\n", i, xk, N(f |> subs(var,x_num)), err)
+                  if i == maxiter
+                        throw(ErrorException("Not Solved!. Iteration limit reached."))
+                        return NaN, NaN
                   end
-
-            x0 = xk
-            x_num = xk
-            i += 1
-
-            if i == maxiter
-                  throw(ErrorException("Not Solved!. Iteration limit reached."))
-                  return NaN, NaN
             end
-      end
-      iter == true ? tf = time_ns() : nothing # To measure algorithm performance
-      iter == true ? println("elapse time: ", (tf - ti)/1.0e9, " seconds") : nothing
+            iter == true ? tf = time_ns() : nothing # To measure algorithm performance
+            iter == true ? println("elapse time: ", (tf - ti)/1.0e9, " seconds") : nothing
 
-      return x_num,  N(f |> subs(var,x_num))
+            return x_num,  N(f |> subs(var,x_num))
+
+      catch Msg
+            println(Msg)
+            return NaN, NaN
+      end
 end
--- src/QuasiNewton.jl
+++ src/QuasiNewton.jl
@@ -6,60 +6,66 @@
        e-mail: kelvyn.baruc@gmail.com
  """ ->
 function QuasiNewton(f, var; x0 = [1 2], error = 0.0001, maxiter = 100, iter = true)
+      try
+            # Symbolic derivatives using Module SymPy
+            dfk1 = diff(f, var)
 
-      # Symbolic derivatives using Module SymPy
-      dfk1 = diff(f, var)
+            err = Inf
+            i = 1
+            xk = 0
+            x_num = 0
 
-      err = Inf
-      i = 1
-      xk = 0
-      x_num = 0
+            # Numeric iterations
+            iter == true ? ti = time_ns() : nothing # To measure algorothm performance
 
-      # Numeric iterations
-      iter == true ? ti = time_ns() : nothing # To measure algorothm performance
+            while err > error
+                  x_num = x0
 
-      while err > error
-            x_num = x0
+                  xk = x_num[2]-(N(dfk1 |> subs(var,x_num[2]))/(N(dfk1 |> subs(var,x_num[2])) -
+                        N(dfk1 |> subs(var,x_num[1]))))*(x_num[2] - x_num[1])
 
-            xk = x_num[2]-(N(dfk1 |> subs(var,x_num[2]))/(N(dfk1 |> subs(var,x_num[2])) -
-                  N(dfk1 |> subs(var,x_num[1]))))*(x_num[2] - x_num[1])
+                  if xk > x_num[2]
+                        err = abs(x_num[1] - xk)
+                        x0 = [x_num[2] xk]
 
-            if xk > x_num[2]
-                  err = abs(x_num[1] - xk)
-                  x0 = [x_num[2] xk]
+                  elseif xk < x_num[1]
+                        err = abs(x_num[2] - xk)
+                        x0 = [xk x_num[1]]
 
-            elseif xk < x_num[1]
-                  err = abs(x_num[2] - xk)
-                  x0 = [xk x_num[1]]
-
-            elseif xk < x_num[2] && xk > x_num[1]
-                   err = abs(x_num[1] - xk)
-                   x0 = [xk x_num[2]]
-            end
-
-                  if iter == true
-                        if i == 1
-                              println("===========================================================")
-                              println(" QuasiNewton Solver for Unconstrained Optimization (0.0.2)")
-                        end
-
-                        if i == 1 || ceil((i/1)/10) == (i/1)/10
-                              println("===========================================================")
-                              @printf("Iter\t X\t\t f(x)\t\t |err|\n")
-                              println("===========================================================")
-                        end
-
-                        @printf("%i\t %4.6e\t %4.6e\t %4.6e\n", i, xk, N(f |> subs(var,xk)), err)
+                  elseif xk < x_num[2] && xk > x_num[1]
+                         err = abs(x_num[1] - xk)
+                         x0 = [xk x_num[2]]
                   end
 
-            if i == maxiter
-                  throw(ErrorException("Not Solved!. Iteration limit reached."))
-                  return NaN, NaN
-            end
+                        if iter == true
+                              if i == 1
+                                    println("===========================================================")
+                                    println(" QuasiNewton Solver for Unconstrained Optimization (0.0.2)")
+                                    println(" ")
+                              end
 
-            i += 1
+                              if i == 1 || ceil((i/1)/10) == (i/1)/10
+                                    println("===========================================================")
+                                    @printf("Iter\t X\t\t f(x)\t\t |err|\n")
+                                    println("===========================================================")
+                              end
+
+                              @printf("%i\t %4.6e\t %4.6e\t %4.6e\n", i, xk, N(f |> subs(var,xk)), err)
+                        end
+
+                  if i == maxiter
+                        throw(ErrorException("Not Solved!. Iteration limit reached."))
+                  end
+
+                  i += 1
+            end
+            iter == true ? tf = time_ns() : nothing # To measure algorithm performance
+            iter == true ? println("elapse time: ", (tf - ti)/1.0e9, " seconds") : nothing
+            
+            return xk,  N(f |> subs(var,xk))
+
+      catch Msg
+            println(Msg)
+            return NaN, NaN
       end
-      iter == true ? tf = time_ns() : nothing # To measure algorithm performance
-      iter == true ? println("elapse time: ", (tf - ti)/1.0e9, " seconds") : nothing
-      return xk,  N(f |> subs(var,xk))
 end
