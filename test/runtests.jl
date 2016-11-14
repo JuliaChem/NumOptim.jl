a8b85e1a36994dbd34d517c7dc17d12823419517
Kelvyn Baruc SS <kelvyn.baruc@gmail.com>
2016-11-14 17:49:28 -0600

    Coveralls

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,23 +2,20 @@ using OptimJulia
 using Base.Test
 using SymPy
 
-x = Sym("x")
-
-f = ((((1 - 8x) + 2*x^2) - (10/3)*x^3) + (1/4)*x^4 + (4/5)*x^5) - (1/6)*x^6
-
-Newton(f, x, 3)
-
 x1,x2 = Sym("x1,x2")
+
+f1 = ((((1 - 8x1) + 2*x1^2) - (10/3)*x1^3) + (1/4)*x1^4 + (4/5)*x1^5) - (1/6)*x1^6
+
+Newton(f1, x1, 3)
+
 vars = [x1,x2]
-f = -log(1 - x1 - x2) - log(x1) - log(x2)
+f2 = -log(1 - x1 - x2) - log(x1) - log(x2)
 x0 = [0.85,0.05]
 
-Newton(f, vars, x0)
+Newton(f2, vars, x0)
 
-f = ((((1 - 8x) + 2*x^2) - (10/3)*x^3) + (1/4)*x^4 + (4/5)*x^5) - (1/6)*x^6
-
- for i = 2:10
-       QuasiNewton(f, x, x0 = [i i+4])
-       println("=========================================")
-       println("Test $i with QuasiNewton algorithm passed")
- end
+for i = 2:10
+      QuasiNewton(f1, x1, x0 = [i i+4])
+      println("=========================================")
+      println("Test $i with QuasiNewton algorithm passed")
+end
